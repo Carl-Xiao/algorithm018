248c8a131248764f7bcaef6c7d26a5b53ec33b1b
carl-Xiao <270555686@qq.com>
2020-11-14 17:18:53 +0800

    add

---
+++ week04/岛屿数量/main.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+// 200. 岛屿数量
+// 给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
+
+//dfs深度优先遍历
+
+func numIslands(grid [][]byte) int {
+	count := 0
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid[i]); j++ {
+			if grid[i][j] == '1' {
+				count++
+				fmt.Println(count)
+				dfs(i, j, grid)
+			}
+
+		}
+	}
+	return count
+}
+
+func dfs(i, j int, grid [][]byte) {
+	if i < 0 || j < 0 || i >= len(grid) || j >= len(grid[i]) || grid[i][j] == '0' {
+		return
+	}
+	grid[i][j] = '0'
+	dfs(i-1, j, grid)
+	dfs(i+1, j, grid)
+	dfs(i, j-1, grid)
+	dfs(i, j+1, grid)
+}
+
+func main() {
+
+}
